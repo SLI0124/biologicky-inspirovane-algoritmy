6e742892222ee056f7b83574f865447f1dc69419
Vojtěch Slíva <sli0124@vsb.cz>
2024-12-05 13:52:35 +0100

    docs(task9): refactor & docs

--- tasks/task9.py
+++ tasks/task9.py
@@ -1,67 +1,54 @@
-"""
-Firefly Algorithm:
-initialize population of fireflies = x_i (i=1,2,...,n)
-set control parameters
-t = 0
-while t < t_max
-    for i in range(n)
-        for j in range(n) 
-            light intensity I_i at x_i is determined by f(x_i)
-            if I_j > I_i
-                move firefly i towards j
-            attractiveness varies with distance r via exp(-gamma r^2)
-            evaluate new solutions and update light intensity
-            
-        rank fireflies and find the current best
-        t += 1
-"""
-from solution import import_and_run
-from tasks.solution import MAX_ITERATIONS
 import numpy as np
 import random
+from solution import import_and_run
+from tasks.solution import MAX_ITERATIONS
 
 # Constants
-POP_SIZE = 20  # population size
-G = 200  # number of generations
-ALPHA = 0.3  # alpha parameter for firefly movement
-BETA_ZERO = 1.0  # minimum beta parameter for firefly movement
-GAMMA = 1.0  # gamma parameter for attractiveness
+POP_SIZE = 20  # Population size
+G = 200  # Number of generations
+ALPHA = 0.3  # Alpha for the firefly movement, that is the randomization parameter
+BETA_ZERO = 1.0  # Beta for the firefly movement, that is the attractiveness parameter
 
 
-def firefly_algorithm(lower_bound, upper_bound, function, max_iteration=MAX_ITERATIONS, d=2, pop_size=POP_SIZE, g_max=G,
-                      alpha=ALPHA, b_0=BETA_ZERO, gamma=GAMMA):
-    def generate_population():
-        population = []
-        for i in range(pop_size):
-            population.append(np.array([random.uniform(lower_bound, upper_bound) for _ in range(d)]))
-        return population
+def generate_population(lower_bound, upper_bound, d, pop_size):
+    """Generate an initial population of fireflies."""
+    return [np.array([random.uniform(lower_bound, upper_bound) for _ in range(d)]) for _ in range(pop_size)]
 
-    def distance(a, b):
-        return np.linalg.norm(a - b)
 
-    def get_best(pop, lights):
-        return pop[lights.index(min(lights))]
+def distance(x1, x2):
+    """Calculate the Euclidean distance between two points."""
+    return np.linalg.norm(x1 - x2)
 
-    def get_light_intensities(p):
-        return [function(x) for x in p]
 
-    def normal():
-        return np.random.normal(0, 1, d)
+def get_best(pop, lights):
+    """Get the best firefly in the population based on light intensity."""
+    return pop[np.argmin(lights)]
 
-    result = []
-    pop = generate_population()
-    lights = get_light_intensities(pop)
 
-    for g in range(g_max):
-        for i in range(pop_size):
-            for j in range(pop_size):
-                if lights[i] > lights[j]:  # If firefly i is brighter than firefly j
-                    pop[i] = pop[i] + (b_0 / (1 + distance(pop[i], pop[j]))) * (pop[j] - pop[i]) + alpha * normal()
+def get_light_intensities(pop, function):
+    """Calculate the light intensities of the fireflies."""
+    return [function(x) for x in pop]
 
-                lights[i] = function(pop[i])
 
-        # Record the best solution for the current generation
-        result.append([list(get_best(pop, lights))])
+def normal(d):
+    """Generate a normally distributed random vector."""
+    return np.random.normal(0, 1, d)
+
+
+def firefly_algorithm(lower_bound, upper_bound, function, max_iteration=MAX_ITERATIONS, d=2, pop_size=POP_SIZE,
+                      g_max=G, alpha=ALPHA, b_0=BETA_ZERO):
+    pop = generate_population(lower_bound, upper_bound, d, pop_size)  # Generate the initial population
+    lights = get_light_intensities(pop, function)  # Calculate the light intensities of the fireflies
+    result = []  # Store the best solution for each generation
+
+    for _ in range(g_max):  # Iterate over the generations
+        for i in range(pop_size):  # Iterate over the fireflies
+            for j in range(pop_size):  # Iterate over the other fireflies
+                if lights[i] > lights[j]:  # If the light intensity of the current firefly is greater than the other
+                    # Update the position of the firefly
+                    pop[i] += (b_0 / (1 + distance(pop[i], pop[j]))) * (pop[j] - pop[i]) + alpha * normal(d)
+            lights[i] = function(pop[i])  # Update the light intensity of the firefly
+        result.append([list(get_best(pop, lights))])  # Store the best solution for the current generation
 
     return result
 
