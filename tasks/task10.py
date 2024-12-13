b09c5c3a037edf92cd5c6306058bc11f6b3a3d48
Vojtech Sliva <sli0124@vsb.cz>
2024-12-13 13:30:28 +0100

    refactor(task10): some code cleanup

--- tasks/task10.py
+++ tasks/task10.py
@@ -17,45 +17,34 @@ NUMBER_OF_EXPERIMENTS = 30
 
 def teaching_learning_based_optimization(lower_bound, upper_bound, test_function, pop_size=POPULATION_SIZE,
                                          dimension=DIMENSION, max_ofe=MAX_OFE):
-    # Initialize population and fitness
     population = np.random.uniform(lower_bound, upper_bound, (pop_size, dimension))
     fitness = np.array([test_function(ind) for ind in population])
-    ofe = pop_size  # Initial evaluations
+    ofe = pop_size
+    results = []
 
-    results = []  # Store population points for each iteration
-
-    # Main optimization loop
     while ofe < max_ofe:
-        # Teaching Phase
         best_idx = np.argmin(fitness)
         teacher = population[best_idx]
-
         mean_population = np.mean(population, axis=0)
         teaching_factor = np.random.randint(1, 3)
 
         new_population = population + np.random.rand(pop_size, dimension) * (
-                teacher - teaching_factor * mean_population
-        )
+                teacher - teaching_factor * mean_population)
         new_population = np.clip(new_population, lower_bound, upper_bound)
-
         new_fitness = np.array([test_function(ind) for ind in new_population])
         ofe += pop_size
 
-        # Accept better solutions
         improved = new_fitness < fitness
         population[improved] = new_population[improved]
         fitness[improved] = new_fitness[improved]
 
-        # Learning Phase
         for i in range(pop_size):
             partner_idx = np.random.choice([idx for idx in range(pop_size) if idx != i])
             partner = population[partner_idx]
-
             direction = partner - population[i] if fitness[partner_idx] < fitness[i] else population[i] - partner
 
             new_individual = population[i] + np.random.rand(dimension) * direction
             new_individual = np.clip(new_individual, lower_bound, upper_bound)
-
             new_individual_fitness = test_function(new_individual)
             ofe += 1
 
@@ -63,75 +52,23 @@ def teaching_learning_based_optimization(lower_bound, upper_bound, test_function
                 population[i] = new_individual
                 fitness[i] = new_individual_fitness
 
-        # Store population points for this iteration
         results.append(population.copy().tolist())
 
     return results
 
 
 def get_best_result(results, test_function):
-    last_population = results[-1]  # Get the last population
-    fitness_values = [test_function(np.array(individual)) for individual in
-                      last_population]  # Ensure it's a NumPy array
-    best_idx = np.argmin(fitness_values)  # Find the index of the best individual
-    best_value = fitness_values[best_idx]  # Get the best fitness value
-    return best_value
+    last_population = results[-1]
+    fitness_values = [test_function(np.array(individual)) for individual in last_population]
+    best_idx = np.argmin(fitness_values)
+    return fitness_values[best_idx]
 
 
-def create_xlsx_file():
-    results = []
+def save_results_to_excel(results, file_path):
+    workbook = xlsxwriter.Workbook(file_path)
 
-    for experiment_number in range(1, NUMBER_OF_EXPERIMENTS + 1):
-        print(f"Experiment {experiment_number}/{NUMBER_OF_EXPERIMENTS}")
-        for i, function in get_all_functions().items():
-            lower_bound, upper_bound, _ = get_function_parameters(function)
-            print(f"Function: {function.__name__}")
-
-            differential_result = differential_evolution(lower_bound=lower_bound, upper_bound=upper_bound,
-                                                         test_function=function, dimension=DIMENSION,
-                                                         pop_size=POPULATION_SIZE, f=F, cr=CR, g_max=G_MAX)
-            dif_best = get_best_result(differential_result, function)
-
-            pso_res = particle_swarm_optimization(lower_bound=lower_bound, upper_bound=upper_bound,
-                                                  test_function=function,
-                                                  pop_size=POPULATION_SIZE, dimension=DIMENSION, m_max=M_MAX, c1=C1,
-                                                  c2=C2, v_max=V_MAX, v_min=V_MIN, w_max=W_MAX, w_min=W_MIN)
-            pso_best = get_best_result(pso_res, function)
-
-            soma_res = self_organizing_migration_algorithm(lower_bound=lower_bound, upper_bound=upper_bound,
-                                                           test_function=function, pop_size=POPULATION_SIZE,
-                                                           dim=DIMENSION, migrations=MIGRATIONS, step=STEP, prt=PRT,
-                                                           path_length=PATH_LENGTH)
-            soma_best = get_best_result(soma_res, function)
-
-            firefly_res = firefly_algorithm(lower_bound=lower_bound, upper_bound=upper_bound, test_function=function,
-                                            d=DIMENSION, pop_size=POPULATION_SIZE, g_max=G, alpha=ALPHA, b_0=BETA_ZERO)
-            firefly_best = get_best_result(firefly_res, function)
-
-            tlbo_res = teaching_learning_based_optimization(lower_bound=lower_bound,
-                                                            upper_bound=upper_bound,
-                                                            test_function=function,
-                                                            pop_size=POPULATION_SIZE,
-                                                            dimension=DIMENSION, max_ofe=MAX_OFE)
-            tlbo_best = get_best_result(tlbo_res, function)
-
-            results.append((experiment_number, function.__name__,
-                            dif_best, pso_best, soma_best, firefly_best, tlbo_best))
-
-        print()
-
-    # create xlsx file
-    save_dir = "../results/"
-    file_name = "task10_results.xlsx"
-
-    if not os.path.exists(save_dir):
-        os.makedirs(save_dir)
-
-    workbook = xlsxwriter.Workbook(save_dir + file_name)
-
-    # create worksheet for each function
-    for function in get_all_functions().values():
-        worksheet = workbook.add_worksheet(function.__name__)
+    for function_name, function_results in results.items():
+        worksheet = workbook.add_worksheet(function_name)
         worksheet.write(0, 0, "Experiment")
         worksheet.write(0, 1, "DE")
         worksheet.write(0, 2, "PSO")
@@ -139,42 +76,62 @@ def create_xlsx_file():
         worksheet.write(0, 4, "FA")
         worksheet.write(0, 5, "TLBO")
 
-    # write results to the xlsx file
-    for result in results:
-        worksheet = workbook.get_worksheet_by_name(result[1])
-        worksheet.write(result[0], 0, f"Exp. {result[0]}")
-        worksheet.write(result[0], 1, result[2])  # Just write the scalar directly
-        worksheet.write(result[0], 2, result[3])  # Same here
-        worksheet.write(result[0], 3, result[4])  # And here
-        worksheet.write(result[0], 4, result[5])  # Same here
-        worksheet.write(result[0], 5, result[6])  # Same here
+        for row_idx, row_data in enumerate(function_results, start=1):
+            worksheet.write_row(row_idx, 0, row_data)
 
-    # Calculate mean and std deviation and put it in the last row
-    for function in get_all_functions().values():
-        worksheet = workbook.get_worksheet_by_name(function.__name__)
-        worksheet.write(NUMBER_OF_EXPERIMENTS + 1, 0, "Mean")
-        worksheet.write(NUMBER_OF_EXPERIMENTS + 2, 0, "Std. Dev")
-
-        # Collect data manually to calculate mean and std. dev
-        data_de = [result[2] for result in results if result[1] == function.__name__]
-        data_pso = [result[3] for result in results if result[1] == function.__name__]
-        data_soma = [result[4] for result in results if result[1] == function.__name__]
-        data_fa = [result[5] for result in results if result[1] == function.__name__]
-        data_tlbo = [result[6] for result in results if result[1] == function.__name__]
-
-        # Write the mean and standard deviation for each column
-        for col, data in enumerate([data_de, data_pso, data_soma, data_fa, data_tlbo], 1):
-            mean = np.mean(data)
-            std_dev = np.std(data)
-            worksheet.write(NUMBER_OF_EXPERIMENTS + 1, col, mean)
-            worksheet.write(NUMBER_OF_EXPERIMENTS + 2, col, std_dev)
+        data = np.array(function_results)[:, 1:]
+        mean_row = ["Mean"] + np.mean(data, axis=0).tolist()
+        std_row = ["Std. Dev"] + np.std(data, axis=0).tolist()
+        worksheet.write_row(len(function_results) + 1, 0, mean_row)
+        worksheet.write_row(len(function_results) + 2, 0, std_row)
 
     workbook.close()
 
 
-if __name__ == "__main__":
-    # uncomment to create and save animations, maybe check and set dimension value to 2
-    # import_and_run(teaching_learning_based_optimization)
+def create_xlsx_file():
+    results = {}
 
-    # uncomment to create xlsx file with all the results
+    for experiment_number in range(1, NUMBER_OF_EXPERIMENTS + 1):
+        print(f"\nExperiment {experiment_number}/{NUMBER_OF_EXPERIMENTS}")
+
+        for function_id, function in get_all_functions().items():
+            lower_bound, upper_bound, _ = get_function_parameters(function)
+            print(f"Function: {function.__name__}")
+
+            differential_result = differential_evolution(lower_bound=lower_bound, upper_bound=upper_bound,
+                                                         test_function=function, dimension=DIMENSION,
+                                                         pop_size=POPULATION_SIZE, f=F, cr=CR, g_max=G_MAX)
+            pso_result = particle_swarm_optimization(lower_bound=lower_bound, upper_bound=upper_bound,
+                                                     test_function=function, pop_size=POPULATION_SIZE,
+                                                     dimension=DIMENSION, m_max=M_MAX, c1=C1, c2=C2, v_max=V_MAX,
+                                                     v_min=V_MIN, w_max=W_MAX, w_min=W_MIN)
+            soma_result = self_organizing_migration_algorithm(lower_bound=lower_bound, upper_bound=upper_bound,
+                                                              test_function=function, pop_size=POPULATION_SIZE,
+                                                              dim=DIMENSION, migrations=MIGRATIONS, step=STEP, prt=PRT,
+                                                              path_length=PATH_LENGTH)
+            firefly_result = firefly_algorithm(lower_bound=lower_bound, upper_bound=upper_bound, test_function=function,
+                                               d=DIMENSION, pop_size=POPULATION_SIZE, g_max=G, alpha=ALPHA,
+                                               b_0=BETA_ZERO)
+            tlbo_result = teaching_learning_based_optimization(lower_bound=lower_bound, upper_bound=upper_bound,
+                                                               test_function=function, pop_size=POPULATION_SIZE,
+                                                               dimension=DIMENSION, max_ofe=MAX_OFE)
+
+            results.setdefault(function.__name__, []).append([
+                experiment_number,
+                get_best_result(differential_result, function),
+                get_best_result(pso_result, function),
+                get_best_result(soma_result, function),
+                get_best_result(firefly_result, function),
+                get_best_result(tlbo_result, function)
+            ])
+
+    save_dir = "../results/"
+    os.makedirs(save_dir, exist_ok=True)
+    save_results_to_excel(results, os.path.join(save_dir, "task10_results.xlsx"))
+
+
+if __name__ == "__main__":
     create_xlsx_file()
+
+    # uncomment this to create animations
+    # import_and_run(teaching_learning_based_optimization)
